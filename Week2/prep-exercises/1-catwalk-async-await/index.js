2531ce8730da059bcc555905d25325c388dafbbe
Yuliia Ozkan <yulka.oak18@gmail.com>
2024-11-09 23:58:06 +0100

    prep-ex done

--- Week2/prep-exercises/1-catwalk-async-await/index.js
+++ Week2/prep-exercises/1-catwalk-async-await/index.js
@@ -5,16 +5,33 @@ const STEP_SIZE_PX = 10;
 const DANCE_TIME_MS = 5000;
 const DANCING_CAT_URL =
   'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+const WALKING_CAT_URL =
+  'http://www.anniemation.com/clip_art/images/cat-walk.gif';
 
 function walk(img, startPos, stopPos) {
   return new Promise((resolve) => {
-    // Copy over the implementation from last week
+    let position = startPos;
+    img.style.left = position + 'px';
+
+    const intervalId = setInterval(() => {
+      position += STEP_SIZE_PX;
+      img.style.left = position + 'px';
+
+      if (position >= stopPos) {
+        clearInterval(intervalId);
+        resolve();
+      }
+    }, STEP_INTERVAL_MS);
   });
 }
 
 function dance(img) {
   return new Promise((resolve) => {
-    // Copy over the implementation from last week
+    img.src = DANCING_CAT_URL;
+    setTimeout(() => {
+      img.src = WALKING_CAT_URL;
+      resolve();
+    }, DANCE_TIME_MS);
   });
 }
 
@@ -24,7 +41,11 @@ async function catWalk() {
   const centerPos = (window.innerWidth - img.width) / 2;
   const stopPos = window.innerWidth;
 
-  // Use async/await syntax to loop the walk and dance functions
+  while (true) {
+    await walk(img, startPos, centerPos);
+    await dance(img);
+    await walk(img, centerPos, stopPos);
+  }
 }
 
 window.addEventListener('load', catWalk);
--- Week2/prep-exercises/2-pokemon-fetch/index.js
+++ Week2/prep-exercises/2-pokemon-fetch/index.js
@@ -11,39 +11,38 @@
  *
  * The HTML file already contains the necessary HTML elements.
  *----------------------------------------------------------------------------*/
-
 const VALID_URL = 'https://pokeapi.co/api/v2/pokemon/?limit=5';
 const INVALID_URL = 'https://pokeapi.co/api/v2/pokemons/?limit=5';
 
 async function fetchJSON(url) {
-  // TODO
+  try {
+    const response = await fetch(url);
 
-  // Fetch the JSON data from the web API that responds to the `url` parameter
-  // and return a promise that resolves to a corresponding JavaScript object.
-  // Make sure to check for HTTP errors.
+    if (!response.ok) {
+      throw new Error(`Cannot fetch the data: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(`Data is not fetched! Error: ${error.message}`);
+  }
 }
 
 function renderResults(pokemons) {
-  // 1. Clear the text content of the HTML element with id `error`.
   const errorElement = document.querySelector('#error');
   errorElement.innerText = '';
 
-  // 2. Set the text content of the HTML element with id `json` to JSON text
-  //    from the `pokemons` argument, formatted in a human readable form (i.e.,
-  //    with indentation and line breaks).
   const pokemonsElement = document.querySelector('#json');
   pokemonsElement.innerText = JSON.stringify(pokemons, null, 2);
 }
 
 function renderError(err) {
-  // 1. Clear the text content of the HTML element with id `json`.
   const pokemonsElement = document.querySelector('#json');
   pokemonsElement.innerText = '';
 
-  // 2. Set the text content of the HTML element with id `error` to the
-  //    `.message` property of the `err` parameter.
   const errorElement = document.querySelector('#error');
-  errorElement.innerText = err;
+  errorElement.innerText = err.message;
 }
 
 function main() {
@@ -52,10 +51,13 @@ function main() {
     const option = document.querySelector('#option');
     const url = option.checked ? INVALID_URL : VALID_URL;
 
-    // TODO
-    // Use `fetchJSON()` to fetch data from the selected url.
-    // If successful, render the data by calling function `renderResults()`.
-    // On failure, render the error by calling function `renderError()`.
+    fetchJSON(url)
+      .then((data) => {
+        renderResults(data);
+      })
+      .catch((error) => {
+        renderError(error);
+      });
   });
 }
 
